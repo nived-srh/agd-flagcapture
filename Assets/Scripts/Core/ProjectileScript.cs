04dee4ac1a2e2001f47af0c7099aa11454dd5f46
Frederick Michael Theohu <fredmt2031@gmail.com>
2023-02-24 12:37:58 +0100

    Flying Enemy prefab ver-3 and make all platform clones have 'Platform' layer

--- Assets/Scripts/Core/ProjectileScript.cs
+++ Assets/Scripts/Core/ProjectileScript.cs
@@ -1,4 +1,4 @@
-    using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,14 +14,15 @@ public class ProjectileScript : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
-
         Vector3 direction = player.transform.position - transform.position;
-        rb.velocity = new Vector2(direction.x, direction.y).normalized * force; 
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         timer += Time.deltaTime;
 
         if (timer > 10)
@@ -30,12 +31,12 @@ public class ProjectileScript : MonoBehaviour
         }
     }
 
-     void OnTriggerEnter2D(Collider2D other)
-     {
+    void OnTriggerEnter2D(Collider2D other)
+    {
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.GetComponent<PlayerHealth>().currentHealth -= 5;
             Destroy(gameObject);
         }
-     }
+    }
 }
