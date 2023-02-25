718497ce0c3e6a3f075e34f38233dca304a217a0
Frederick Michael Theohu <fredmt2031@gmail.com>
2023-02-25 20:14:35 +0100

    Sandworm trap

--- Assets/Scripts/Core/Traps/Firetrap.cs
+++ Assets/Scripts/Core/Traps/Firetrap.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Firetrap : MonoBehaviour
 {
     Animator animator;
+    private PlayerHealth playerHealth;
+    public float damage;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -26,6 +30,14 @@ public class Firetrap : MonoBehaviour
         {
             animator.ResetTrigger("isAttacking");
             animator.SetTrigger("isIdle");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerHealth.TakeDamage(damage);
         }
     }
 }
